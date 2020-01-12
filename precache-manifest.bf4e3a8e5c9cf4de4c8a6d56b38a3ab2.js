eb6d6437bc4a6f425d24325a4422bb901d6d15e9
Yota.K <powdersugar828828@gmail.com>
2020-01-12 20:24:27 +0900

    Updates

---
+++ precache-manifest.bf4e3a8e5c9cf4de4c8a6d56b38a3ab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a82ac6a71e85ed11a5dbc410d2c0b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ee01312c22e33d05673",
+    "url": "/static/css/main.a0fdb11e.chunk.css"
+  },
+  {
+    "revision": "7a6fe6f41972eb004f22",
+    "url": "/static/js/2.571f2ade.chunk.js"
+  },
+  {
+    "revision": "2ee01312c22e33d05673",
+    "url": "/static/js/main.aa849dc5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
